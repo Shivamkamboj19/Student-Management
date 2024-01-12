3ee45d10fe5d9bb399150775f09c86530b7356c4
Shivam Kamboj <shivamkamboj631@gmail.com>
2024-01-13 02:09:22 +0530

    navbar changes and deleted unnecessary files

---
+++ student-management/src/Pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "../../Components/AppComponents/Navbar";
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default HomePage;
